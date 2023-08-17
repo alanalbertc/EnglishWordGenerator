c53d328cb8c04f340c7e2a56cfecf6ef55eab98b
alanalbertc <68207532+alanalbertc@users.noreply.github.com>
2023-08-17 16:11:10 -0600

    Se agrego el navigation rail. MyHomePage class se cambio a un statefulWidget para trackear el valor del indice seleccionado en el navigation rail.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -28,23 +29,77 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
   var favorites = <WordPair>[];
-  
-  void getNext(){
+
+  void getNext() {
     current = WordPair.random();
     notifyListeners();
   }
 
-  void toogleFavorite(){
+  void toogleFavorite() {
     if (favorites.contains(current)) {
       favorites.remove(current);
-    }else{
+    } else {
       favorites.add(current);
     }
     notifyListeners();
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('Widget no implementado para $selectedIndex');
+    }
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+                child: NavigationRail(
+              extended: constraints.maxWidth >= 600,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text("Home"),
+                ),
+                NavigationRailDestination(
+                    icon: Icon(Icons.favorite), label: Text("Favorites")),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });
+              },
+            )),
+            Expanded(
+                child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            ))
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -53,41 +108,37 @@ class MyHomePage extends StatelessWidget {
     IconData heartIcon;
     if (appState.favorites.contains(pair)) {
       heartIcon = Icons.favorite;
-    }else{
+    } else {
       heartIcon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton(
-                  onPressed: (){
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
                     appState.getNext();
                   },
-                  child: Text("Next")
-                ),
-                SizedBox(width: 20),
-                //like button
-                ElevatedButton.icon(
-                  onPressed: (){
-                    appState.toogleFavorite();
-                    print("lista de favoritos: ${appState.favorites}");//degub
-                  }, 
-                  label: Text("Like"),
-                  icon: Icon(heartIcon),
-                ),
-              ],
-            ),
-          ],
-        ),
+                  child: Text("Next")),
+              SizedBox(width: 20),
+              //like button
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toogleFavorite();
+                  print("lista de favoritos: ${appState.favorites}"); //degub
+                },
+                label: Text("Like"),
+                icon: Icon(heartIcon),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
